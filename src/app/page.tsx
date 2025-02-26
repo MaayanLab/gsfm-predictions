ea771d4c4e15e5af745ebc1ca6f8c731184418ae
Daniel Clarke <u8sand@gmail.com>
2025-02-26 16:43:26 -0500

    Add images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,18 @@ function range(n: number) {
   return L
 }
 
+const icons = {
+  LINCS_L1000_Chem_Pert_Consensus_Sigs: 'https://cfde-drc.s3.amazonaws.com/assets/img/LINCS-logo.png',
+  LINCS_L1000_CRISPR_KO_Consensus_Sigs: 'https://cfde-drc.s3.amazonaws.com/assets/img/LINCS-logo.png',
+  LINCS_L1000_Consensus_Median_Signatures: 'https://cfde-drc.s3.amazonaws.com/assets/img/LINCS-logo.png',
+  ARCHS4_IDG_Coexp: 'https://data.cfde.cloud/img/IDG.png',
+  GlyGen_Glycosylated_Proteins_2022: 'https://cfde-drc.s3.amazonaws.com/assets/img/glygen.png',
+  GTEx_Aging_Signatures_2021: 'https://data.cfde.cloud/img/GTEx.png',
+  GTEx_Tissues_V8_2023: 'https://data.cfde.cloud/img/GTEx.png',
+  IDG_Drug_Targets_2022: 'https://data.cfde.cloud/img/IDG.png',
+  MoTrPAC_Endurance_Trained_Rats_2023: 'https://data.cfde.cloud/img/MoTrPAC.png',
+}
+
 function Predictions(props: { source: string, gene: string, count: number }) {
   const pageSize = 8
   const [page, setPage] = React.useState(1)
@@ -21,7 +33,12 @@ function Predictions(props: { source: string, gene: string, count: number }) {
   })
   return (
     <div className="border rounded-xl p-4 w-96 flex flex-col overflow-hidden">
-      <h3 className="text-wrap">{props.source.replaceAll('_', ' ')}</h3>
+      <div className="flex flex-row gap-2 align-center items-center">
+        <div className="w-24 h-24 flex items-center">
+          <img src={icons[props.source]} alt={props.source} />
+        </div>
+        <h3 className="text-wrap">{props.source.replaceAll('_', ' ')}</h3>
+      </div>
       <div className="overflow-auto flex-grow">
         <table className="table w-full">
           <thead>
