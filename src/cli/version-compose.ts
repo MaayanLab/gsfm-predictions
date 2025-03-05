f7f6a21b9aff47c76990ccc93b219741f8aa24de
Daniel Clarke <u8sand@gmail.com>
2025-03-05 14:35:41 -0500

    0.1.2

--- src/cli/version-compose.ts
+++ src/cli/version-compose.ts
@@ -1,6 +1,5 @@
 import fs from 'fs'
-import path from 'path'
 
-let compose = fs.readFileSync(path.join(__dirname, '..', '..', 'docker-compose.yaml'), { encoding: 'utf-8' })
-compose = compose.replaceAll(/maayanlab\/gsfm-app:[\d\.]+/g, `maayanlab/gsfm-app:${process.env.npm_package_version}`)
-fs.writeFileSync(path.join(__dirname, '..', 'docker-compose.yaml'), compose)
+let compose = fs.readFileSync('docker-compose.yaml', { encoding: 'utf-8' })
+compose = compose.replaceAll(/maayanlab\/gsfm:[\d\.]+/g, `maayanlab/gsfm:${process.env.npm_package_version}`)
+fs.writeFileSync('docker-compose.yaml', compose)
