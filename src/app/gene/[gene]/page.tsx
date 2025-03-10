dd67e477af1814ea62fe832b24ad25c86ad1db91
Daniel Clarke <u8sand@gmail.com>
2025-03-10 17:24:41 -0400

    Add icon

--- src/app/gene/[gene]/page.tsx
+++ src/app/gene/[gene]/page.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import trpc from '@/lib/trpc/server'
 import Link from 'next/link';
+import iconSvg from '@/app/icon.svg'
 import { Waypoints } from '@/components/waypoint';
 import AllPredictions from '@/components/gene/AllPredictions';
 import { notFound } from 'next/navigation';
 import ReactMarkdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
+import Image from 'next/image';
 
 export default async function Home({ params }: { params: Promise<{ gene: string }> }) {
   const gene_info = await trpc.gene_info((await params).gene)
@@ -60,8 +62,9 @@ export default async function Home({ params }: { params: Promise<{ gene: string 
         {models.map(({ model }) => 
           <div key={model} className="prose max-w-full border border-t-0 border-secondary rounded-b-lg p-4">
             <div className="flex flex-row">
-              {/* <img src={undefined} alt="GSFM" /> */}
-              <div className="w-24 h-24 border m-4 self-center">&nbsp;</div>
+              <div className="w-32 m-8 self-center place-self-center">
+                <Image unoptimized objectFit='contain' src={iconSvg} alt="GSFM" />
+              </div>
               <div className="flex flex-col">
                 <h2>GSFM gene annotation predictions ({model})</h2>
                 <p>The gene annotations below have been generated using GSFM. GSFM uses is an auto-encoder-like deep machine learning model trained on gene sets from supplemental material of literature. More information about the method can be found <Link href="/about">here</Link>.</p>
