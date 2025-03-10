0cd598011ddd4cac3380eca376a45849e511f638
Daniel Clarke <u8sand@gmail.com>
2025-03-10 19:09:16 -0400

    Sort sources by CFDE|not + count

--- src/app/router.ts
+++ src/app/router.ts
@@ -3,6 +3,29 @@ import { procedure, router } from "@/lib/trpc";
 import { sql } from "kysely";
 import { z } from 'zod'
 
+const source_tags: Record<string, string[]> = {
+  LINCS_L1000_Chem_Pert_Consensus_Sigs: ['CFDE'],
+  LINCS_L1000_CRISPR_KO_Consensus_Sigs: ['CFDE'],
+  LINCS_L1000_Consensus_Median_Signatures: ['CFDE'],
+  ARCHS4_IDG_Coexp: ['CFDE'],
+  GlyGen_Glycosylated_Proteins_2022: ['CFDE'],
+  GTEx_Aging_Signatures_2021: ['CFDE'],
+  GTEx_Tissues_V8_2023: ['CFDE'],
+  IDG_Drug_Targets_2022: ['CFDE'],
+  MoTrPAC_Endurance_Trained_Rats_2023: ['CFDE'],
+  KOMP2_Mouse_Phenotypes_2022: ['CFDE'],
+  ChEA_2022: [],
+  KEA_2015: [],
+  Human_Phenotype_Ontology: [],
+  GWAS_Catalog_2023: [],
+  GO_Biological_Process_2023: [],
+  KEGG_2021_Human: [],
+  MGI_Mammalian_Phenotype_Level_4_2024: [],
+  OMIM_Disease: [],
+  HuBMAP_Azimuth: ['CFDE'],
+  HuBMAP_ASCTpB: ['CFDE'],
+}
+
 export default router({
   gene_info: procedure.input(z.string()).query(async (props) => {
     return await db
@@ -31,13 +54,19 @@ export default router({
     model: z.string().default('latest'),
     gene: z.string(),
   })).query(async (props) => {
-    return await db
+    const sources = (await db
       .selectFrom('app.prediction')
       .select(eb => ['source', eb.fn.countAll().as('count')])
       .groupBy('source')
       .where('model', '=', props.input.model)
       .where('gene', '=', props.input.gene)
       .execute()
+    ).map(({ source, count }) => ({ source, count, tags: source_tags[source] ?? [] }))
+    sources.sort((a, b) =>
+      (b.tags.length - a.tags.length)
+      || (Number(b.count) - Number(a.count))
+    )
+    return sources
   }),
   predictions: procedure.input(z.object({
     model: z.string().default('latest'),
