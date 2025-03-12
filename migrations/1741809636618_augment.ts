0c67e4d3f85cfd95f332219de58505c23f270448
Daniel Clarke <u8sand@gmail.com>
2025-03-12 16:09:36 -0400

    Add table for augment function

---
+++ migrations/1741809636618_augment.ts
@@ -0,0 +1,27 @@
+import { type Kysely } from 'kysely'
+
+export async function up(db: Kysely<any>): Promise<void> {
+	await db.schema.withSchema('app')
+		.createTable('user_gene_set')
+		.addColumn('id', 'uuid', col => col.primaryKey())
+		.addColumn('gene_set', 'varchar', col => col.notNull())
+		.addColumn('description', 'varchar')
+		.execute()
+	await db.schema.withSchema('app')
+		.createTable('user_prediction')
+		.addColumn('id', 'bigserial', col => col.primaryKey())
+		.addColumn('gene_set', 'uuid', col => col.references('app.user_gene_set.id').onDelete('cascade'))
+		.addColumn('model', 'varchar', c => c.notNull().defaultTo('latest'))
+    .addColumn('gene', 'varchar', c => c.notNull())
+    .addColumn('proba', 'float4', c => c.notNull())
+		.execute()
+}
+
+export async function down(db: Kysely<any>): Promise<void> {
+	await db.schema.withSchema('app')
+		.dropTable('user_prediction')
+		.execute()
+	await db.schema.withSchema('app')
+		.dropTable('user_gene_set')
+		.execute()
+}
