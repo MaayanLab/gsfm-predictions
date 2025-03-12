b7727a4637fd20044d96681f0162f72cbb68136d
Daniel Clarke <u8sand@gmail.com>
2025-03-12 15:48:20 -0400

    Make header more like DRC portal

--- src/app/downloads/page.tsx
+++ src/app/downloads/page.tsx
@@ -1,7 +1,6 @@
 export default function Downloads() {
   return (
     <main className="container mx-auto flex flex-col gap-4 items-stretch grow">
-      Downloads
     </main>
   )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,17 +5,20 @@ import Image from "next/image"
 import Link from "next/link"
 import iconSvg from '@/app/icon.svg'
 import { useParams, usePathname, useRouter } from "next/navigation"
+import classNames from "classnames"
 
 export default function Header() {
   const router = useRouter()
   const pathname = usePathname()
   const params = useParams<{ gene?: string }>()
   return (
-    <header className="my-2 px-2">
+    <header className="container mx-auto my-2 px-2">
       <div className="navbar bg-base-100 gap-2">
         <div className="flex-1">
-          <Link className="flex flex-row justify-start align-middle gap-2" href="/">
-            <Image className="w-6" src={iconSvg} alt="GSFM Logo" unoptimized />
+          <Link className="flex flex-row justify-start align-middle items-center gap-2" href="/">
+            <div className="shadow-md rounded-full w-12 h-12 p-1 flex">
+              <Image src={iconSvg} alt="GSFM Logo" unoptimized />
+            </div>
             <span className="font-bold text-xl lg:text-3xl xl:text-4xl text-primary">
               Gene Set Foundation Model (GSFM)
             </span>
@@ -28,27 +31,11 @@ export default function Header() {
               router.push(`/gene/${encodeURIComponent(value)}`)
             }}
           />}
-          <div className="dropdown dropdown-end">
-            <div tabIndex={0} role="button" className="btn btn-square btn-ghost">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                className="inline-block h-5 w-5 stroke-current">
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path>
-              </svg>
-            </div>
-            <ul
-              tabIndex={0}
-              className="menu menu-lg dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow-sm">
-              <li><a href="/downloads">Downloads</a></li>
-            </ul>
-          </div>
         </div>
+      </div>
+      <div className="flex flex-row flex-wrap gap-8">
+        <Link className={classNames("text-primary font-bold", { 'border-b-2': pathname === '/' || pathname.startsWith('/gene/') })} href="/">SEARCH</Link>
+        <Link className={classNames("text-primary font-bold", { 'border-b-2': pathname === '/downloads' })} href="/downloads">DOWNLOADS</Link>
       </div>
     </header>
   )
