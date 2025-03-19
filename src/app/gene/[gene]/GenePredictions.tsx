eeab6dfd43a45e2c40ee21e3617835c9bee35647
Daniel J. B. Clarke <u8sand@gmail.com>
2025-03-19 17:18:58 -0400

    Tweak predictions page a bit

--- src/app/gene/[gene]/GenePredictions.tsx
+++ src/app/gene/[gene]/GenePredictions.tsx
@@ -1,5 +1,4 @@
 import trpc from '@/lib/trpc/server'
-import Link from 'next/link';
 import ModelPredictions from './ModelPredictions';
 import React from 'react';
 
@@ -14,30 +13,19 @@ const icons = {
 
 const descriptions = {
   rummagene: <>gene sets from supplemental material of literature from <a href="https://rummagene.com" target='_blank'>Rummagene</a></>,
-  rummageo: <>differentially expressed genes in GEO studies from <a href="https://rummageo.com" target='_blank'>Rummageo</a></>,
-  rummage: <>gene sets from supplemental material of literature from <a href="https://rummagene.com" target='_blank'>Rummagene</a> and differentially expressed genes in GEO studies from <a href="https://rummageo.com" target='_blank'>Rummageo</a></>,
+  rummageo: <>differentially expressed genes in GEO studies from <a href="https://rummageo.com" target='_blank'>RummaGEO</a></>,
+  rummage: <>gene sets from supplemental material of literature from <a href="https://rummagene.com" target='_blank'>Rummagene</a> and differentially expressed genes in GEO studies from <a href="https://rummageo.com" target='_blank'>RummaGEO</a></>,
 } as Record<string, React.ReactNode>
 
 export default async function GenePredictions(props: { gene: string }) {
   const models = await trpc.models()
   return <>
     {models.map(({ model }) => 
-      <div key={model} className="prose max-w-full border border-t-0 border-secondary rounded-b-lg p-4">
-        <div className="flex flex-row">
-          <div className="w-64 m-8 self-center place-self-center">
-            {model in icons ?
-              typeof icons[model] === 'string' ? <img src={icons[model]} alt={model} />
-              : icons[model]
-            : null}
-          </div>
-          <div className="flex flex-col">
-            <h2>GSFM gene annotation predictions{models.length > 1 && <>&nbsp;({model})</>}</h2>
-            <p>
-              The gene annotations below have been generated using GSFM.
-              GSFM is an auto-encoder-like deep machine learning model trained on {descriptions[model] ?? 'gene sets'}.
-            </p>
-          </div>
-        </div>
+      <div key={model} className="prose max-w-full border border-t-0 border-secondary rounded-b-lg p-4 px-6">
+        <h2 className="text-4xl mb-2">GSFM gene annotation predictions{models.length > 1 && <>&nbsp;({model})</>}</h2>
+        <p>
+          The gene annotations below have been generated using GSFM, an auto-encoder-like deep machine learning model trained on {descriptions[model] ?? 'gene sets'}.
+        </p>
         <ModelPredictions model={model} />
       </div>
     )}
