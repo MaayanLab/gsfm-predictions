e52e376a8a9635aa6d39760636d3a0c0ed80547a
Daniel J. B. Clarke <u8sand@gmail.com>
2025-04-04 11:53:24 -0400

    Center header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,19 +12,19 @@ export default function Header() {
   const pathname = usePathname()
   const params = useParams<{ gene?: string }>()
   return (
-    <header className="container mx-auto my-2 px-2">
-      <div className="navbar bg-base-100 gap-2">
-        <div className="flex-1">
+    <header className="mx-auto my-2 px-2">
+      <div className="navbar bg-base-100 gap-x-10 flex-wrap justify-center">
+        <div>
           <Link className="flex flex-row justify-start align-middle items-center gap-2" href="/">
             <div className="shadow-md rounded-full w-12 h-12 p-1 flex">
               <Image src={iconSvg} alt="GSFM Logo" unoptimized />
             </div>
-            <span className="font-bold text-xl lg:text-3xl xl:text-4xl text-primary">
-              Gene Set Foundation Model (GSFM)
+            <span className="font-bold text-2xl sm:text-3xl md:text-4xl text-primary whitespace-nowrap mb-0">
+              Gene Set Foundation Model
             </span>
           </Link>
         </div>
-        <div className="flex-none flex flex-row gap-2 place-items-center">
+        <div className="flex-none flex flex-row gap-2 my-2 place-items-center">
           {pathname !== '/' && <GeneInput
             value={params.gene ?? ''}
             onChange={value => {
@@ -33,7 +33,7 @@ export default function Header() {
           />}
         </div>
       </div>
-      <div className="flex flex-row flex-wrap gap-8">
+      <div className="flex flex-row flex-wrap gap-8 justify-center">
         <Link className={classNames("text-primary font-bold", { 'border-b-2': pathname === '/' || pathname.startsWith('/gene/') })} href="/">SEARCH</Link>
         <Link className={classNames("text-primary font-bold", { 'border-b-2': pathname === '/augment' })} href="/augment" >AUGMENT</Link>
         <Link className={classNames("text-primary font-bold", { 'border-b-2': pathname === '/downloads' })} href="/downloads">DOWNLOADS</Link>
