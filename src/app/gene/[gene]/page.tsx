16aefc504742fc484beeda7813ae8098e3cb0d4b
Daniel Clarke <u8sand@gmail.com>
2025-04-17 10:51:18 -0400

    Add page titles, and improved meta

--- src/app/gene/[gene]/page.tsx
+++ src/app/gene/[gene]/page.tsx
@@ -3,12 +3,26 @@ import trpc from '@/lib/trpc/server'
 import { Waypoints } from '@/components/waypoint';
 import GeneInfo from './GeneInfo';
 import GenePredictions from './GenePredictions';
-import { notFound } from 'next/navigation';
+import { notFound, redirect } from 'next/navigation';
+import { Metadata } from 'next';
+
+const getGeneInfo = React.cache(async (gene: string) => trpc.gene_info(gene))
+
+export async function generateMetadata(props: { params: Promise<{ gene: string }> }): Promise<Metadata> {
+  const gene = (await props.params).gene
+  const gene_info = await getGeneInfo(gene)
+  if (!gene_info) notFound()
+  else if (gene_info.symbol !== gene) redirect(`/gene/${gene_info.symbol}`)
+  return {
+    title: `${gene}`,
+  }
+}
 
 export default async function Home({ params }: { params: Promise<{ gene: string }> }) {
   const gene = (await params).gene
-  const gene_info = await trpc.gene_info(gene)
+  const gene_info = await getGeneInfo(gene)
   if (!gene_info) notFound()
+  else if (gene_info.symbol !== gene) redirect(`/gene/${gene_info.symbol}`)
   return (
     <Waypoints>
       <GeneInfo gene_info={gene_info} />
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,41 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
-  title: "GSFM",
-  description: "Gene Set Foundation Model",
+  title: {
+    template: 'GSFM | %s',
+    default: 'GSFM'
+  },
+  description: "Predict gene function with Gene Set Foundation Model (GSFM)",
+  keywords: [
+    'big data',
+    'bioinformatics',
+    'cancer',
+    'cell line',
+    'data',
+    'dataset',
+    'diabetes',
+    'disease',
+    'drug discovery',
+    'drug',
+    'gene set library',
+    'gene set',
+    'gene',
+    'genomics',
+    'machine learning',
+    'pharmacology',
+    'phenotype',
+    'protein',
+    'proteomics',
+    'RNA-seq',
+    'RNAseq',
+    'systems biology',
+    'target discovery',
+    'target',
+    'therapeutics',
+    'transcriptomics',
+    'gene function',
+    'predictions',
+  ].join(', ')
 };
 
 export default function RootLayout({
