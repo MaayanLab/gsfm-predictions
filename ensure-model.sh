901f21379cf0f52898a898f9fbb583dac0301fca
Daniel Clarke <u8sand@gmail.com>
2025-09-29 18:38:08 -0400

    Download all models on startup

--- ensure-model.sh
+++ ensure-model.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
-mkdir -p model
-if [ ! -f model/vocab.txt ]; then curl -L https://s3.dev.maayanlab.cloud/gsfm/vocab.txt -o model/vocab.txt; fi
-if [ ! -f model/gsfm.onnx ]; then curl -L https://s3.dev.maayanlab.cloud/gsfm/gsfm.onnx -o model/gsfm.onnx; fi
+mkdir -p data/model/gsfm-rummagene
+if [ ! -f data/model/gsfm-rummagene/vocab.txt ]; then curl -L https://s3.dev.maayanlab.cloud/gsfm/gsfm-rummagene/vocab.txt -o data/model/gsfm-rummagene/vocab.txt; fi
+if [ ! -f data/model/gsfm-rummagene/gsfm.onnx ]; then curl -L https://s3.dev.maayanlab.cloud/gsfm/gsfm-rummagene/gsfm.onnx -o data/model/gsfm-rummagene/gsfm.onnx; fi
+mkdir -p data/model/gsfm-rummageo
+if [ ! -f data/model/gsfm-rummageo/vocab.txt ]; then curl -L https://s3.dev.maayanlab.cloud/gsfm/gsfm-rummageo/vocab.txt -o data/model/gsfm-rummageo/vocab.txt; fi
+if [ ! -f data/model/gsfm-rummageo/gsfm.onnx ]; then curl -L https://s3.dev.maayanlab.cloud/gsfm/gsfm-rummageo/gsfm.onnx -o data/model/gsfm-rummageo/gsfm.onnx; fi
+mkdir -p data/model/gsfm-rummage
+if [ ! -f data/model/gsfm-rummage/vocab.txt ]; then curl -L https://s3.dev.maayanlab.cloud/gsfm/gsfm-rummage/vocab.txt -o data/model/gsfm-rummage/vocab.txt; fi
+if [ ! -f data/model/gsfm-rummage/gsfm.onnx ]; then curl -L https://s3.dev.maayanlab.cloud/gsfm/gsfm-rummage/gsfm.onnx -o data/model/gsfm-rummage/gsfm.onnx; fi
 
-MODEL_PATH=$(pwd)/model $@
+MODEL_PATH=$(pwd)/data/model $@
