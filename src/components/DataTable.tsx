599004e60d9c38a60ec534eaed84fde00d8c1fef
Daniel Clarke <u8sand@gmail.com>
2026-01-26 16:15:04 -0500

    Improvements to DataTable

--- src/components/DataTable.tsx
+++ src/components/DataTable.tsx
@@ -2,7 +2,7 @@
 import classNames from 'classnames'
 import React from 'react'
 
-export default function DataTable<C extends object>(props: { columns: { [k in keyof C]: { th: React.ReactNode, td: (datum: C[k]) => React.ReactNode } }, data: { [k in keyof C]: C[k] }[], defaultOrderBy: `${string & keyof C} asc` | `${string & keyof C} desc` }) {
+export default function DataTable<C extends object>(props: { title?: React.ReactElement, columns: { [k in keyof C]: { th: React.ReactNode, td: (datum: C[k]) => React.ReactNode } }, data: { [k in keyof C]: C[k] }[], defaultOrderBy: `${string & keyof C} asc` | `${string & keyof C} desc` }) {
   const pageSize = 10
   const totalCount = props.data.length
   const [page, setPage] = React.useState(1)
@@ -29,45 +29,44 @@ export default function DataTable<C extends object>(props: { columns: { [k in ke
     return view.slice((page-1)*pageSize, page*pageSize)
   }, [props.data, page, filter, orderBy, pageSize])
   return (
-    <div className="flex flex-col place-items-center gap-2">
-      <div className="flex flex-row self-stretch justify-end">
+    <div className="flex flex-col place-items-start gap-2 m-4">
+      <div className="flex flex-row self-stretch justify-between items-center">
+        <span className="text-primary">{props.title}</span>
         <label className={classNames("input input-bordered flex items-center rounded-full text-primary font-semibold not-focus-within:w-28")}>
           <img src="/resources/SearchIcon.svg" alt="" />
           <input type="text" className="grow not-focus:placeholder:text-primary" placeholder="Search" value={filter} onChange={evt => {setFilter(() => evt.target.value); setPage(1)}} />
         </label>
       </div>
-      <div className="overflow-x-auto">
-        <table className="table table-sm text-primary">
-          <thead>
-            <tr>
-              {Object.keys(props.columns).map((col) => {
-                const column = props.columns[col as keyof C]
-                return (
-                  <th key={col} className="text-primary text-center">
-                    <button
-                      className="cursor-pointer"
-                      onClick={evt => {setOrderBy(orderBy => orderBy === `${col as string & keyof C} desc` ? `${col as string & keyof C} asc` : `${col as string & keyof C} desc`); setPage(() => 1)}}
-                    >
-                      {column.th}
-                      {orderBy === `${col} asc` ? <>&uarr;</>
-                        : orderBy === `${col} desc` ? <>&darr;</>
-                        : <span className="invisible">&darr;</span>}
-                    </button>
-                  </th>
-                )
-              })}
-            </tr>
-          </thead>
-          <tbody>
-            {view.map((datum, i) => <tr key={i}>
-              {Object.keys(props.columns).map((col, j) => {
-                const column = props.columns[col as keyof C]
-                return <td key={j}>{column.td(datum[col as keyof C])}</td>
-              })}
-            </tr>)}
-          </tbody>
-        </table>
-      </div>
+      <table className="table table-sm text-primary">
+        <thead className="bg-[#F9FAFE]">
+          <tr>
+            {Object.keys(props.columns).map((col) => {
+              const column = props.columns[col as keyof C]
+              return (
+                <th key={col} className="text-primary text-center">
+                  <button
+                    className="cursor-pointer"
+                    onClick={evt => {setOrderBy(orderBy => orderBy === `${col as string & keyof C} desc` ? `${col as string & keyof C} asc` : `${col as string & keyof C} desc`); setPage(() => 1)}}
+                  >
+                    {column.th}
+                    {orderBy === `${col} asc` ? <>&uarr;</>
+                      : orderBy === `${col} desc` ? <>&darr;</>
+                      : <span className="invisible">&darr;</span>}
+                  </button>
+                </th>
+              )
+            })}
+          </tr>
+        </thead>
+        <tbody>
+          {view.map((datum, i) => <tr key={i}>
+            {Object.keys(props.columns).map((col, j) => {
+              const column = props.columns[col as keyof C]
+              return <td key={j} className={classNames('text-center', {  'bg-[#DCEBFF]': i%2==0 })}>{column.td(datum[col as keyof C])}</td>
+            })}
+          </tr>)}
+        </tbody>
+      </table>
       <div className="join items-center justify-center gap-1">
         {page > 2 && <button className="join-item btn text-[#6992C8] bg-white border font-normal border-[#6992C8] rounded-lg" onClick={evt => {setPage(page => 1)}}>1</button>}
         {page > 3 && <button className="join-item btn text-[#6992C8] bg-white border font-normal border-[#6992C8] rounded-lg btn-disabled">...</button>}
